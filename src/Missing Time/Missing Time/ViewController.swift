145a956d27f3c3741dbdf5bd24431e3196c20fe3
Brian Welsh <welshbr@outlook.com>
2017-05-26 05:29:04 -0500

    Create code project shell as reference point

---
+++ src/Missing Time/Missing Time/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Missing Time
+//
+//  Created by Brian Welsh on 5/26/17.
+//  Copyright © 2017 Brian Welsh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
